6659b125320b7b87f88cdeb269b21a1f7f7f7efd
KaMaDi <artkamadi@gmail.com>
2024-06-16 08:04:54 +0300

    Initial Commit

---
+++ Todo+JSONph/Todo_JSONphApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_JSONphApp.swift
+//  Todo+JSONph
+//
+//  Created by Jedi on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_JSONphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
